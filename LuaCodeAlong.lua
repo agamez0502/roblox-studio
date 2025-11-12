2da6b810b614981116955cde44e36df5c75c93f8
Alondra Gamez <agamez@appdev.yearup.org>
2025-11-12 15:43:59 -0600

    getting this to work so that character can walk around tiles

---
+++ LuaCodeAlong.lua
@@ -0,0 +1,31 @@
+
+--game.Players.PlayerAdded:Connect(function(player)
+    --print("Welcome to the game " .. player.Name)
+
+
+
+--local discoTile =  game.Workspace.DiscoFloor.Part
+local DiscoFloorModel = game.Workspace.DiscoFloor
+local partIsTouched = false
+
+local groupTiles = DiscoFloorModel:getChildren()
+
+-- tiles into a table 
+
+
+
+-- check if all tiles are being touched 
+-- if player touches red tile, then back to spawn
+
+
+local function DiscoTiles(touch)
+
+    local playerTouched = game.Players:GetPlayerFromCharacter(touch.Parent)
+    if playerTouched then
+        print(playerTouched.Name .. " touched the part!")
+    end
+end
+
+for i=1, #groupTiles do
+    groupTiles[i].Touched:Connect(DiscoTiles)
+end
