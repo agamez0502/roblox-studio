3038b361b3d43612da5a649ac0df27d9ec7d6033
Alondra Gamez <agamez@appdev.yearup.org>
2025-11-12 16:01:10 -0600

    checking if all tiles are being touched and if player touches blue tile then we get some reference to that

--- LuaCodeAlong.lua
+++ LuaCodeAlong.lua
@@ -2,8 +2,6 @@
 --game.Players.PlayerAdded:Connect(function(player)
     --print("Welcome to the game " .. player.Name)
 
-
-
 --local discoTile =  game.Workspace.DiscoFloor.Part
 local DiscoFloorModel = game.Workspace.DiscoFloor
 local partIsTouched = false
@@ -11,18 +9,17 @@ local partIsTouched = false
 local groupTiles = DiscoFloorModel:getChildren()
 
 -- tiles into a table 
-
-
-
 -- check if all tiles are being touched 
 -- if player touches red tile, then back to spawn
-
 
 local function DiscoTiles(touch)
 
     local playerTouched = game.Players:GetPlayerFromCharacter(touch.Parent)
     if playerTouched then
         print(playerTouched.Name .. " touched the part!")
+        if touch.Name == "PartBlue" then 
+            print("something")
+        end
     end
 end
 
